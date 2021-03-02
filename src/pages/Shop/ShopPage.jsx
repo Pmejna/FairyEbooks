f83d1142af29f6d93103b78033647299df5f1d17
Przemyslaw Mejna <pmejna@gmail.com>
2021-03-02 23:32:25 +0000

    create shop page component, create dummy-data js file

---
+++ src/pages/Shop/ShopPage.jsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import styled from 'styled-components';
+
+class ShopPage extends React.PureComponent {
+    constructor(props) {
+        super(props);
+            this.state = {
+                collections: null,
+            }
+        }
+
+        render() {
+        return (
+            <>
+            </>
+        )
+    }
+};
+
+export default ShopPage
