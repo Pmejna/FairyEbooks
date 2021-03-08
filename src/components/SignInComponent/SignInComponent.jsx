e636173e0b291e3c9ecb608f6d33778843f5074e
Przemyslaw Mejna <pmejna@gmail.com>
2021-03-08 14:08:06 +0000

    create SignIn page, SignUp and SignIn components

---
+++ src/components/SignInComponent/SignInComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+class SignUpComponent extends React.PureComponent {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+};
+
+export default SignUpComponent
---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+class SignUp extends React.PureComponent {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+};
+
+export default SignUp
